81ebc779d0731dc9d2946924fe839e947cf0f38f
Balajiw920 <wbalaji2021@gmail.com>
2023-04-05 02:03:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+      for (let i = 0; i < 5; i++) {
+        promises.push(
+          new Promise((resolve, reject) => {
+            const time = Math.floor(Math.random() * 5000) + 1000;
+            setTimeout(() => {
+              resolve(`Promise ${i + 1} resolved in ${time} ms`);
+            }, time);
+          })
+        );
+      }
+
+      Promise.any(promises)
+        .then((result) => {
+          const outputDiv = document.getElementById("output");
+          outputDiv.innerHTML = result;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
